ccee16155436b2020e412f231ca13d94f37e050f
ano <250081373@qq.com>
2017-03-30 13:07:42 +0800

    clear alex

--- dep/alex.lua
+++ dep/alex.lua
@@ -2,8 +2,6 @@ function buildalex(featsize)
 	local SpatialConvolution = cudnn.SpatialConvolution
 	local SpatialMaxPooling = cudnn.SpatialMaxPooling
 
-	-- from https://code.google.com/p/cuda-convnet2/source/browse/layers/layers-imagenet-1gpu.cfg
-	-- this is Net that was presented in the One Weird Trick paper. http://arxiv.org/abs/1404.5997
 	local alex = nn.Sequential()
 		:add(SpatialConvolution(3,64,11,11,4,4,2,2))		 -- 224 -> 55
 		:add(SpatialMaxPooling(3,3,2,2))						 -- 55 ->  27
@@ -30,27 +28,16 @@ function buildalex(featsize)
 		for k,v in pairs(alex:findModules(name)) do
 			local n = v.kW*v.kH*v.nOutputPlane
 			v.weight:normal(0,math.sqrt(2/n))
-			if cudnn.version >= 4000 then
-				v.bias = nil
-				v.gradBias = nil
-			else
-				v.bias:zero()
-			end
+			v.bias = nil
+			v.gradBias = nil
 		end
 	end
-	local function BNInit(name)
-		for k,v in pairs(alex:findModules(name)) do
-			v.weight:fill(1)
-			v.bias:zero()
-		end
-	end
+
 	ConvInit('cudnn.SpatialConvolution')
-	ConvInit('nn.SpatialConvolution')
-	BNInit('cudnn.SpatialBatchNormalization')
-	BNInit('nn.SpatialBatchNormalization')
 	for k,v in pairs(alex:findModules('nn.Linear')) do
 		v.bias:zero()
 	end
+
 	alex:get(1).gradInput = nil
 
 	return alex
